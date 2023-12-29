6dbebbdb08339eeb8f960a1490c349a49150382c
marcelcausone <marcel_causone@hotmail.com>
2023-12-29 15:12:17 -0300

    Last commit

--- client/src/views/detail/detail.component.jsx
+++ client/src/views/detail/detail.component.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, Link } from "react-router-dom";
 import { getCountriesByID, cleanDetail } from "../../redux/actions";
-import  style from  "./detail.module.css";
+import style from "./detail.module.css";
 
 const Detail = () => {
   const dispatch = useDispatch();
@@ -23,36 +23,40 @@ const Detail = () => {
   return (
     <div className={style.modalbackground}>
       <div className={style.modalcontainer}>
-        <div >
-        <Link to="/home">
-          <button className={style.modalclosebutton}>&times;</button>
-        </Link>
+        <div>
+          <Link to="/home">
+            <button className={style.modalclosebutton}>&times;</button>
+          </Link>
         </div>
-        <div className={style.image}>
-        {allCountriesDetail?.flagImage && (
-          <img
-          style={{borderRadius:"15px"}}
-            src={allCountriesDetail.flagImage}
-            alt={allCountriesDetail.name}
-          />
-        )}
+        <div className={style.detailContent}>
+          <div className={style.image}>
+            {allCountriesDetail?.flagImage && (
+              <img
+                style={{ borderRadius: "15px" }}
+                src={allCountriesDetail.flagImage}
+                alt={allCountriesDetail.name}
+              />
+            )}
+          </div>
+          <div className={style.text}>
+            <h1>{allCountriesDetail?.name}</h1>
+            <h2>ID: {allCountriesDetail?.ID}</h2>
+            <h2>Continet: {allCountriesDetail?.continent}</h2>
+            <h2>Capital: {allCountriesDetail?.capital}</h2>
+            <h2>Area: {allCountriesDetail?.area} KM </h2>
+            <h2>
+              Population: {allCountriesDetail?.population} habitantes
+            </h2>
+            <h2>Subregion: {allCountriesDetail?.subregion}</h2>
+          </div>
         </div>
-      <div className={style.text}>
-      <h1>{allCountriesDetail?.name}</h1>
-        <h2>ID: {allCountriesDetail?.ID}</h2>
-        <h2>Continet: {allCountriesDetail?.continent}</h2>
-        <h2>Capital: {allCountriesDetail?.capital}</h2>
-        <h2>Area: {allCountriesDetail?.area} KM </h2>
-        <h2>Population: {allCountriesDetail?.population} habitantes</h2>
-        <h2>Subregion: {allCountriesDetail?.subregion}</h2>
-      </div>
 
-        <button style={{margin:"none"}} onClick={toggleActivities}>
+        <button style={{ margin: "none" }} onClick={toggleActivities}>
           {showActivities ? "Hide Activities" : "Show Activities"}
         </button>
         {showActivities && (
-          <>
-            <h2>Activities:</h2>
+         <div className={style.activities + (showActivities ? ` ${style.active}` : "")}>
+      
             <ul>
               {allCountriesDetail?.Activities?.map((activity) => (
                 <li key={activity.id}>
@@ -62,7 +66,7 @@ const Detail = () => {
                 </li>
               ))}
             </ul>
-          </>
+          </div>
         )}
       </div>
     </div>
